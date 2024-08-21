16aa8601d4fe3c30d36abf2335852922108da60a
Soutlen <tt5tt5291@gmail.com>
2024-08-21 20:06:25 +0700

    firs commit

---
+++ threeScreenApp/threeScreenApp/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  threeScreenApp
+//
+//  Created by Soutlen on 20.08.2024.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let gradientLayer = CAGradientLayer() // Переносим создание градиента на уровень класса
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Задаем цвета градиента
+        let color1 = UIColor(hex: "8CC0F1")?.cgColor ?? UIColor.clear.cgColor
+        let color2 = UIColor(hex: "C0DAB7")?.cgColor ?? UIColor.clear.cgColor
+
+        gradientLayer.colors = [color1, color2] // Два цвета для градиента
+
+        // Задаем направление градиента: сверху вниз
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+
+        // Добавляем градиент на view
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Обновляем размер градиента, чтобы он соответствовал размеру view
+        gradientLayer.frame = self.view.bounds
+    }
+}
+
+// Добавление расширения UIColor для поддержки HEX
+extension UIColor {
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
+
+
+
+
